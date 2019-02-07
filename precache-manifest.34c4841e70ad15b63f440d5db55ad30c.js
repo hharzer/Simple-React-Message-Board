930519b472ef839691974763de7174a8661130b4
pieterlems <pieterlems.personal@protonmail.com>
2019-02-07 22:44:37 +0100

    Updates

---
+++ precache-manifest.34c4841e70ad15b63f440d5db55ad30c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57385e07abd09a06d3c1",
+    "url": "/Simple-React-Message-Board/static/js/runtime~main.57385e07.js"
+  },
+  {
+    "revision": "0f9da2c75e930bc8a3b9",
+    "url": "/Simple-React-Message-Board/static/js/main.0f9da2c7.chunk.js"
+  },
+  {
+    "revision": "1a99b4ce19c44138320b",
+    "url": "/Simple-React-Message-Board/static/js/1.1a99b4ce.chunk.js"
+  },
+  {
+    "revision": "0f9da2c75e930bc8a3b9",
+    "url": "/Simple-React-Message-Board/static/css/main.e1795506.chunk.css"
+  },
+  {
+    "revision": "24ee4d17ef8b5b2e7dfa7035ba5ac86e",
+    "url": "/Simple-React-Message-Board/index.html"
+  }
+];
