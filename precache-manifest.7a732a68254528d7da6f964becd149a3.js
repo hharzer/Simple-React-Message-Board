0148d39720fd3bb72df7739e1a2e86fbda274c55
pieterlems <pieterlems.personal@protonmail.com>
2019-06-05 17:36:06 +0200

    Updates

---
+++ precache-manifest.7a732a68254528d7da6f964becd149a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "452cb1d37da7839a9de4",
+    "url": "/Simple-React-Message-Board/static/css/main.1e11e4cc.chunk.css"
+  },
+  {
+    "revision": "452cb1d37da7839a9de4",
+    "url": "/Simple-React-Message-Board/static/js/main.452cb1d3.chunk.js"
+  },
+  {
+    "revision": "c2265b9132a3c78bdd7e",
+    "url": "/Simple-React-Message-Board/static/js/1.c2265b91.chunk.js"
+  },
+  {
+    "revision": "57385e07abd09a06d3c1",
+    "url": "/Simple-React-Message-Board/static/js/runtime~main.57385e07.js"
+  },
+  {
+    "revision": "a9221cb137cace277cb1de7f2c2bf6c9",
+    "url": "/Simple-React-Message-Board/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8e33aa08779b792c6a9da7ba7e73b70.js"
+  "/Simple-React-Message-Board/precache-manifest.7a732a68254528d7da6f964becd149a3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Simple-React-Message-Board/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
